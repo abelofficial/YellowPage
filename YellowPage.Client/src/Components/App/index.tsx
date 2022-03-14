ba5572a98ef027f76562c0f3a397a8ec1dfe0c50
Abel Sintaro <abelofficial94@gmail.com>
2022-03-14 20:19:09 +0100

    Initial commit.

---
+++ YellowPage.Client/src/Components/App/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <h1> The Yellow Page</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
